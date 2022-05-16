5a5cc111719821bcc19a5cf0fe8ce1d859d91d48
natnael meshesha <natnael.a.meshesha@gmail.com>
2022-05-16 08:41:14 +0000

    new commit

---
+++ main_getline.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * main - uses getline and prints line
+ * Return: 0
+ */
+int main (void)
+{
+	char *line = NULL;
+
+	line = _getline(stdin, line);
+	printf("%s\n", line);
+	free(line);
+	return (0);
+}
